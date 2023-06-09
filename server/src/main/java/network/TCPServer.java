eaeb0fa5940087dd19bb1b35b28798a9742ce0ed
ilya:) <kirillilya.kovalenko@yandex.ru>
2023-06-09 19:50:45 +0300

    :0

--- server/src/main/java/network/TCPServer.java
+++ server/src/main/java/network/TCPServer.java
@@ -1,38 +1,50 @@
 package network;
 
-
-import org.slf4j.LoggerFactory;
-import requests.Request;
-import utils.*;
 import commands.Command;
+import utils.Executor;
+import requests.Request;
+import utils.CollectionHandler;
 
-import java.util.HashMap;
-import java.io.*;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-import ch.qos.logback.classic.*;
-public class TCPServer{
+import java.util.HashMap;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class TCPServer {
     private ServerSocketChannel serverSocketChannel;
-    private int port = 4444;
+    private int port = 3333;
     protected SocketChannel clientSocket;
-    private static final Logger logger = (Logger) LoggerFactory.getLogger(TCPServer.class);
-    public void start(HashMap<String, Command> map, CollectionHandler collectionHandler){
+    private Logger logger = Logger.getLogger("logger");
+    BlockingQueue<String> messageQueue = new LinkedBlockingQueue<>();
+    BlockingQueue<Request> requestQueue = new LinkedBlockingQueue<>();
+    BlockingQueue<Throwable> errorQueue = new LinkedBlockingQueue<>();
+
+    public void start(HashMap<String, Command> map, CollectionHandler collectionHandler) {
         openServerSocket();
-        logger.info("waiting for connection...");
-        while(serverSocketChannel!=null){
-            try{
+        ForkJoinPool pool = new ForkJoinPool();
+        while (serverSocketChannel != null) {
+            logger.log(Level.INFO, "Ожидание подключения...");
+            try {
                 this.clientSocket = serverSocketChannel.accept();
-                logger.info("successfully connected");
-                processRequest(map);
+                logger.log(Level.FINER, "Подключение успешно");
+                pool.execute(new RequestHandler(map, clientSocket, logger, requestQueue, errorQueue));
+                pool.execute(new OutputSocketWriter(clientSocket.socket(), messageQueue, errorQueue));
+                pool.execute(new Executor(map, clientSocket.socket(), requestQueue, messageQueue, errorQueue));
             } catch (IOException ioe) {
-               logger.error("Connection error", ioe.getMessage());
-            } catch (ClassNotFoundException ioe) {
-                logger.error("Request error",ioe.getMessage());
+                logger.log(Level.SEVERE, "Не удалось подключиться к клиенту: ", ioe.getMessage());
             }
         }
         closeServerSocket();
+        pool.shutdown();
     }
 
     private void openServerSocket() {
@@ -40,7 +52,7 @@ public class TCPServer{
             serverSocketChannel = ServerSocketChannel.open();
             serverSocketChannel.bind(new InetSocketAddress("localhost", port));
         } catch (IOException e) {
-            logger.error("Port error", e.getMessage());
+            logger.log(Level.SEVERE,"Ошибка при открытии соединения", e.getMessage());
         }
     }
 
@@ -48,24 +60,54 @@ public class TCPServer{
         try {
             serverSocketChannel.close();
         } catch (IOException e) {
-            logger.error("Closing Port error", e.getMessage());
+            logger.log(Level.SEVERE,"Ошибка при закрытии соединения", e.getMessage());
         }
-    }
-
-    private boolean processRequest(HashMap<String, Command> map) throws IOException, ClassNotFoundException {
-        ObjectInput objectInput = new ObjectInputStream(clientSocket.socket().getInputStream());
-        Request request = (Request) objectInput.readObject();
-        if(map.containsKey(request.getCommandName())){
-            logger.info("Requested command: "+request.getCommandName());
-            map.get(request.getCommandName()).execute(request);
-        } else {
-            logger.error("Unknown command");
-        }
-        objectInput.close();
-        return true;
     }
 
     public Socket getClientSocket(){
         return clientSocket.socket();
+    }
+
+    private static class RequestHandler implements Runnable {
+        private HashMap<String, Command> map;
+        private SocketChannel clientSocket;
+        private Logger logger;
+        BlockingQueue<Request> requestQueue;
+        BlockingQueue<Throwable> errorQueue;
+
+        public RequestHandler(HashMap<String, Command> map, SocketChannel clientSocket, Logger logger, BlockingQueue<Request> requestQueue, BlockingQueue<Throwable> errorQueue) {
+            this.map = map;
+            this.clientSocket = clientSocket;
+            this.logger = logger;
+            this.requestQueue = requestQueue;
+            this.errorQueue = errorQueue;
+        }
+
+        @Override
+        public void run() {
+            try {
+                processRequest(map);
+            } catch (Throwable e) {
+                logger.log(Level.SEVERE, "Ошибка при обработке запроса: " + e.getMessage());
+                try {
+                    errorQueue.put(e);
+                    requestQueue.put(new Request("err", "err", null, null));
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+
+        private boolean processRequest(HashMap<String, Command> map) throws IOException, ClassNotFoundException {
+            ObjectInput objectInput = new ObjectInputStream(clientSocket.socket().getInputStream());
+            Request request = (Request) objectInput.readObject();
+            // objectInput.close();
+            try {
+                requestQueue.put(request);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return true;
+        }
     }
 }

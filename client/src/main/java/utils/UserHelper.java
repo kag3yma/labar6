905636094481281591adaa1d1737796397d83f12
ilya:) <kirillilya.kovalenko@yandex.ru>
2023-06-20 17:11:51 +0300

    превзойду свои лимиты

--- client/src/main/java/utils/UserHelper.java
+++ client/src/main/java/utils/UserHelper.java
@@ -67,7 +67,6 @@ public class UserHelper {
             if (login.trim().equals("Now you are able to use all commands. Use help to see all available commands".trim())){
                 return true;
             } else {
-                System.out.println("пес");
                 ask(scanner);
             }
         }return ask(scanner);
